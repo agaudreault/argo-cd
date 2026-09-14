e3c6698e644cedc66d6771f8d8b81b81fa13b6b8
Alexandre Gaudreault <alexandre_gaudreault@intuit.com>
2026-09-14 18:17:08 -0400

    revert
    Signed-off-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>

--- ui/src/app/typings.d.ts
+++ ui/src/app/typings.d.ts
@@ -2,7 +2,3 @@ declare let SYSTEM_INFO: {version: string};
 // suppress TS7016: Could not find a declaration file for module
 declare module 'react-diff-view';
 declare module 'unidiff';
-// Style side-effect imports are handled by webpack loaders at build time; declare
-// them so the TypeScript language server does not report TS2882 for scss/css imports.
-declare module '*.scss';
-declare module '*.css';
